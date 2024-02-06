bbc986386e92d4204a0a2f3da1d93cc87c5737a7
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-05 22:59:20 -0500

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 
 # Configuración de la base de datos
 DATABASE = 'database.db'
+
+# Función para conectarse a la base de datos
+def connect_db():
+    return sqlite3.connect(DATABASE)
